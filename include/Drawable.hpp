b90a85f4399e2dd06630d7f7707119e176980d17
rrajvardhan <rajvardhantomar25@gmail.com>
2025-12-26 06:13:05 +0530

    erasor tool : use strokes for intersection detection of path

--- include/Drawable.hpp
+++ include/Drawable.hpp
@@ -130,5 +130,27 @@ struct Path : Drawable
     return path.boundingRect().adjusted(-w, -w, w, w);
   }
 
-  bool intersects(const QPainterPath& other) const override { return other.intersects(path); }
+  bool intersects(const QPainterPath& other) const override
+  {
+    // Create a stroked version of this path
+    qreal w = pen.widthF();
+    if (w <= 0)
+      w = 1.0;
+
+    QPainterPathStroker stroker;
+    stroker.setWidth(w);
+    QPainterPath strokedPath = stroker.createStroke(path);
+
+    // Check if any point from the eraser is inside the stroked path
+    for (int i = 0; i < other.elementCount(); ++i)
+    {
+      QPainterPath::Element e = other.elementAt(i);
+      QPointF               point(e.x, e.y);
+
+      if (strokedPath.contains(point))
+        return true;
+    }
+
+    return false;
+  }
 };
